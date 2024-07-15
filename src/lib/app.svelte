c4eb0461ab252832cb5eb6628d58373d13e2516a
Damon Vestervand <damon@vestervand.net>
2024-07-15 12:15:31 +0200

    Type import error example

---
+++ src/lib/app.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { FooBar } from "$lib";
+import ComponentA from "./component-a.svelte";
+
+let fooBar: FooBar;
+let component: ComponentA;
+</script>
+
+<ComponentA fooBar={FooBar.FOO} />
---
+++ src/lib/component-a.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+import type { FooBar } from "$lib";
+
+export let fooBar: FooBar;
+</script>
+
+<div>{fooBar}</div>
